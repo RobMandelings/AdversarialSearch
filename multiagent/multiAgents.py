7b4f1b35f1bf1e11761d77ac5ff803740293a9cb
Rob Mandelings <rob@minetronic.com>
2021-11-18 12:38:19 +0100

    Added EvaluationFunction but working hardly

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -10,13 +10,34 @@
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
+from math import sqrt
 
 from util import manhattanDistance
 from game import Directions
+import enum
 import random, util
 
 from game import Agent
+
+class EvaluationFactors(enum.Enum):
+    """
+    EvaluationFactors describe the factors that participate in calculating the evaluation output. These are used to describe different 'levels' of penalties in the
+    ReflexAgent
+    DISTANCE_TO_FOOD: distance to the food
+    DANGER_LEVEL: safe move, the ghost cannot reach pacman when pacman moves to the location
+    DIRECTION: a weird factor at first sight, but is useful to be determine in which direction to go when directions are regarded as equally valued
+    """
+    DISTANCE_TO_FOOD = enum.auto()
+    DANGER_LEVEL = enum.auto()
+    DIRECTION = enum.auto()
+
+class FoodDistance(enum.Enum):
+    CLOSER = 0
+    SAME = 1
+    FURTHER = 2
+
+def distance(first: tuple, second: tuple) -> float:
+    return sqrt(pow(first[0] - second[0], 2) + pow(first[1] - second[1], 2))
 
 class ReflexAgent(Agent):
     """
@@ -27,7 +48,6 @@ class ReflexAgent(Agent):
     it in any way you see fit, so long as you don't touch our method
     headers.
     """
-
 
     def getAction(self, gameState):
         """
@@ -45,7 +65,7 @@ class ReflexAgent(Agent):
         scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
         bestScore = max(scores)
         bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+        chosenIndex = random.choice(bestIndices)  # Pick randomly among the best
 
         "Add more of your code here if you want to"
 
@@ -67,14 +87,52 @@ class ReflexAgent(Agent):
         to create a masterful evaluation function.
         """
         # Useful information you can extract from a GameState (pacman.py)
+
+        output = 0
+
         successorGameState = currentGameState.generatePacmanSuccessor(action)
         newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood()
+
+        previousFoodPositions = currentGameState.getFood().asList()
+        newFoodPositions = successorGameState.getFood().asList()
+
+        # Does not correspond to new ghost action; Reflex agent does not know what the next action of the ghost will be, so take this into acount.
         newGhostStates = successorGameState.getGhostStates()
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
+        penalties = {EvaluationFactors.DANGER_LEVEL: 100, EvaluationFactors.DISTANCE_TO_FOOD: 10, EvaluationFactors.DIRECTION: 1}
+
+        actionValues = {'North': 0, 'East': 1, 'South': 2, 'West': 3, 'Stop': 4}
+
+        # Houdt ineens rekening met het feit dat meerdere danger positions van meerdere ghosts nog meer gevaarlijk is
+        for ghostState in newGhostStates:
+            ghostPos = ghostState.configuration.getPosition()
+
+            danger = distance(newPos, ghostPos) <= 1
+            output -= int(danger) * penalties.get(EvaluationFactors.DANGER_LEVEL)
+
+        if len(newFoodPositions) != len(previousFoodPositions):
+            foodDistance = FoodDistance.CLOSER
+        else:
+
+            prevFoodDistances = [distance(foodPos, currentGameState.getPacmanPosition()) for foodPos in previousFoodPositions]
+            newFoodDistances = [distance(foodPos, newPos) for foodPos in newFoodPositions]
+
+            minNewFoodDistance = min(newFoodDistances)
+            minPrevFoodDistance = min(prevFoodDistances)
+
+            if minNewFoodDistance < minPrevFoodDistance:
+                foodDistance = FoodDistance.CLOSER
+            elif minNewFoodDistance == minPrevFoodDistance:
+                foodDistance = FoodDistance.SAME
+            else:
+                foodDistance = FoodDistance.FURTHER
+
+        output -= foodDistance.value * penalties.get(EvaluationFactors.DISTANCE_TO_FOOD)
+        output -= actionValues[action] * penalties.get(EvaluationFactors.DIRECTION)
+
         "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+        return output
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -101,8 +159,8 @@ class MultiAgentSearchAgent(Agent):
     is another abstract class.
     """
 
-    def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
-        self.index = 0 # Pacman is always agent index 0
+    def __init__(self, evalFn='scoreEvaluationFunction', depth='2'):
+        self.index = 0  # Pacman is always agent index 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
 
